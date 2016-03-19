ac116a523f2d25e9fe13fd5613915adba12a35ea
Yoga Bayu Aji Pranawa <khususlain@yahoo.com>
2016-03-20 01:37:50 +0700

    First Commit 1. Get Lattitude 2. Get Longitude 3. Get Accuracy

---
+++ app/src/main/java/com/pranawa/gpstracker/AndroidGPSTrackingActivity.java
@@ -0,0 +1,52 @@
+package com.pranawa.gpstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AndroidGPSTrackingActivity extends Activity {
+
+    Button btnShowLocation;
+
+    // GPSTracker class
+    GPSTracker gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_gpstracking);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(AndroidGPSTrackingActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    double acc=gps.getAccuracy();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude + "\nAcc: " + acc, Toast.LENGTH_SHORT).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+    }
+
+}
